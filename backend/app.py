079c069ec0dd0f56028c8a7bb48e698072e1e972
Sammy Baransi <sammy.baransi@hp.com>
2026-09-13 20:01:04 +0300

    Add docker compose

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 import psycopg2
+from flask import request
 
 app = Flask(__name__)
 CORS(app)
@@ -18,12 +19,13 @@ def home():
 def products():
 
     conn = psycopg2.connect(
-        host="host.docker.internal",
-        port="5433",
+        host="postgres",
+        port="5432",
         database="shopdb",
         user="shopuser",
         password="shoppass"
     )
+
 
     cur = conn.cursor()
 
@@ -43,6 +45,32 @@ def products():
         for row in rows
     ]
 
+@app.route("/products", methods=["POST"])
+def create_product():
+
+    data = request.json
+
+    conn = psycopg2.connect(
+        host="host.docker.internal",
+        port="5433",
+        database="shopdb",
+        user="shopuser",
+        password="shoppass"
+    )
+
+    cur = conn.cursor()
+
+    cur.execute(
+        "INSERT INTO products (name, price) VALUES (%s, %s)",
+        (data["name"], data["price"])
+    )
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return {"message": "Product created"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,60 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
-  useEffect(() => {
+  const loadProducts = () => {
     fetch("http://localhost:5002/products")
       .then((res) => res.json())
-      .then((data) => setProducts(data))
-      .catch((err) => console.error(err));
+      .then((data) => setProducts(data));
+  };
+
+  useEffect(() => {
+    loadProducts();
   }, []);
+
+  const addProduct = async () => {
+    await fetch("http://localhost:5002/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        price: Number(price),
+      }),
+    });
+
+    setName("");
+    setPrice("");
+
+    loadProducts();
+  };
 
   return (
     <div style={{ padding: "40px" }}>
       <h1>Shop V2 Products</h1>
+
+      <div>
+        <input
+          placeholder="Product Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          placeholder="Price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+
+        <button onClick={addProduct}>
+          Add Product
+        </button>
+      </div>
+
+      <hr />
 
       {products.map((product) => (
         <div key={product.id}>
